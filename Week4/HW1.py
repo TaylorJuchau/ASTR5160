911e64078a7f2682d72209a2a5ec7d44259e29ec
Taylor Juchau-Peraza <hawkofthemorning@gmail.com>
2025-02-18 15:00:40 -0700

    added HW1 files

--- Week4/HW1.py
+++ Week4/HW1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[38]:
 
 
 import matplotlib.pyplot as plt
@@ -14,8 +14,15 @@ from tabulate import tabulate
 import argparse
 
 
-data_file_location = '/d/scratch/ASTR5160/week4/HW1quasarfile.txt' #Define data file location
+data_file_location = '/d/scratch/ASTR5160/week4/HW1quasarfile.txt' #TJ Define default data file location
+
+#TJ allow file to be run on my laptop by checking if I currently have access to the /d/ drive, if not, define data file location locally
+try: 
+    open(data_file_location, "r")
+except FileNotFoundError:
+    data_file_location = "C:/Users/tj360/ASTR5160/Data_files/HW1_data" #TJ local data files stored in this folder (not synced to github)
 locations = [] #TJ Initialize what will be the full list of 1111 locations
+
 with open(data_file_location, "r") as file:
     lines = file.readlines()  #TJ Read out data from each line
     for line in lines:
@@ -27,12 +34,13 @@ with open(data_file_location, "r") as file:
         #TJ parse each string to extract degrees, min, sec from Dec, make sure to count the sign in front and return it to the 
         sign = -1 if location_strings[1][0] == '-' else 1 #record the sign of the declination
         Dec = f'{int(location_strings[1][1:3]) * sign}' + 'd' + f'{int(location_strings[1][3:5])}' + 'm' + f'{float(location_strings[1][5:])}'+'s'
-        locations.append([RA, Dec])
-locations = np.array(locations)
-QSO_locations = SkyCoord(locations[:,0], locations[:,1], unit = (u.hourangle, u.deg), frame='icrs')
+        locations.append([RA, Dec]) #TJ add to array of all locations in 'xxhxxmxx.xs', 'xxdxxmxx.xs' format
+locations = np.array(locations) #TJ convert to numpy array for better manipulation later
+QSO_locations = SkyCoord(locations[:,0], locations[:,1], unit = (u.hourangle, u.deg), frame='icrs') #TJ convert location array to SkyCoord array
 Observing_location = EarthLocation.of_site('kpno') #TJ set observation location to Kitt Peak, in cartesian coordinates with units of meters
-utcoffset = -7 * u.hour  #TJ define offset from UTC to Mountain time
+
 def find_lowest_airmass(argument):
+    
     '''Find quasars that have the lowest air mass for every day of a given month, when observed from Kitt Peak at 11pm MST
     
     Parameters
@@ -50,19 +58,20 @@ def find_lowest_airmass(argument):
     object Declination in degrees
     airmass of object at 11pm MST when viewed from Kitt Peak Observatory
     '''
-    year = 2025
-    if type(argument) == int:
-        if argument < 1 or argument > 13:
+    
+    year = 2025 #TJ set year to 2025, this can be easilly added as an argument, but assignment specifies that this is only for this year
+    if type(argument) == int: #TJ check if argument is an integer, if it is, check that it is a valid month (between 1 and 12)
+        if argument < 1 or argument > 12:
             print('integer month must be between 1-12')
             return None
         month = argument
-    elif type(argument) == str and len(argument) == 3:
+    elif type(argument) == str and len(argument) == 3: #TJ check if argument is a string, and is in the accepted list of strings
         arg_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         if argument not in arg_list:
             print('month not in recognizable format: acceptable arguments are integers between 1-12, or three letter abreviation of month, or full month. \nString arguments must start with a capital letter')
             return None
         month = arg_list.index(argument)+1
-    elif type(argument) == str and len(argument) > 3:
+    elif type(argument) == str and len(argument) > 3: #TJ check if month is spelled out entirely
         arg_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
         if argument not in arg_list:
             print('month not in recognizable format: acceptable arguments are integers between 1-12, or three letter abreviation of month, or full month. \nString arguments must start with a capital letter')
@@ -71,38 +80,58 @@ def find_lowest_airmass(argument):
     else:
         print('month not in recognizable format: acceptable arguments are integers between 1-12, or three letter abreviation of month, or full month. \nString arguments must start with a capital letter')
         return None
-    month_lengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    month_length = month_lengths[month-1]
-    start_time = Time(f"{year}-{month}-02 06:00:00") +utcoffset
-    rows = [["Date", "Quasar Coordinates (hms.ss ◦ ′ ′′)", "RA (◦)", "Dec (◦)", "Airmass"]]
+    month_lengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] #TJ assign lengths of each month
+    month_length = month_lengths[month-1] #TJ assign the appropriate number of days in the given month
+
+    rows = [["Date", "Quasar Coordinates \n(hhmmss.ss ◦ ′ ′′)", "RA \nDegrees (◦)", "Dec \nDegrees (◦)", "Airmass"]]
+    #TJ Wow FUCK daylight savings time! this just got a whole lot harder... hopefully I did this correctly.
     for day in range(0,month_length):
-        obs_time = start_time + day*24*u.hour
+        if month < 3 or month > 11:
+            utcoffset = -7 * u.hour  #TJ define offset from UTC to Mountain time outside of daylight savings
+        elif month > 3 and month < 11:
+            utcoffset = -6 * u.hour  #TJ define offset from UTC to Mountain time during daylight savings
+        elif month == 3 and day < 8:
+            utcoffset = -7 * u.hour  #TJ define offset from UTC to Mountain time outside of daylight savings
+        elif month == 3 and day > 7:
+            utcoffset = -6 * u.hour  #TJ define offset from UTC to Mountain time during daylight savings
+        elif month == 11 and day < 1:
+            utcoffset = -6 * u.hour  #TJ define offset from UTC to Mountain time outside of daylight savings
+        elif month == 11 and day > 0:
+            utcoffset = -7 * u.hour  #TJ define offset from UTC to Mountain time during daylight savings
+        start_time = Time(f"{year}-{month}-02 06:00:00") + utcoffset #TJ assign start date each time depending on if Daylight savings time is on.
+        obs_time = start_time + day*24*u.hour #TJ observation time is a set number of days after start time (not 24 hours after previous time because fuck daylight savings)
         QSO_altaz_tonight = QSO_locations.transform_to(AltAz(obstime=obs_time, location=Observing_location)) #TJ get altitude and azimuth at this night
-        airmass_array = QSO_altaz_tonight.secz #get airmass at this time
+        airmass_array = QSO_altaz_tonight.secz #TJ get airmass at this time
         positive_airmass_indices = np.where(airmass_array > 0)[0]  #TJ Get indices of positive values for airmass
-        prime_object_index = positive_airmass_indices[np.argmin(airmass_array[positive_airmass_indices])]
-        time = obs_time.value
-        prime_object_raw_location = lines[prime_object_index].strip()
-        RA, Dec = QSO_locations[prime_object_index].ra, QSO_locations[prime_object_index].dec
-        min_air_mass = airmass_array[prime_object_index]
-        rows.append([time, prime_object_raw_location, RA, Dec, min_air_mass])
-    print(tabulate(rows[1:], headers = rows[0]))
+        prime_object_index = positive_airmass_indices[np.argmin(airmass_array[positive_airmass_indices])] #TJ extract lowest positive airmass
+        time = Time(f"{year}-{month}-{day+1} 11:00:00") #TJ set obs_time manually because daylight savings time screws up just using obs_time.value
+        prime_object_raw_location = lines[prime_object_index].strip() #TJ get raw location in given units for table
+        RA, Dec = QSO_locations[prime_object_index].ra.deg, QSO_locations[prime_object_index].dec.deg #TJ get RA and Dec in normal person units
+        min_air_mass = airmass_array[prime_object_index] #TJ get airmass for table
+        rows.append([time, prime_object_raw_location, RA, Dec, min_air_mass]) #TJ add this day's data to table
+    print(tabulate(rows[1:], headers = rows[0])) #TJ print table
     
     return None
 
-
+#TJ make sure this part runs when called from the command line
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Find lowest airmass quasars for each day of a given month.")
-    parser.add_argument("month", type=str, help="Month as an integer (1-12) or name (e.g., 'Jan', 'January')")
-    args = parser.parse_args()
-    
-    find_lowest_airmass(args.month)
+    parser = argparse.ArgumentParser(description="Find lowest airmass quasars for each day of a given month.") #TJ add argparse object and description
+    parser.add_argument("month", type=str, help="Month as an integer (1-12) or name (e.g., Jan, January)") #TJ assign argument type and help note
+    args = parser.parse_args() #TJ assign arugments
+
+    #TJ try to convert argument to integer, if that fails, assume it is a month name
+    try:
+        month_input = int(args.month)
+    except ValueError:
+        month_input = args.month  
+
+    find_lowest_airmass(month_input) #TJ run the function
 
 
-# In[2]:
+# In[ ]:
 
 
-find_lowest_airmass(1)
+
 
 
 # In[ ]:
