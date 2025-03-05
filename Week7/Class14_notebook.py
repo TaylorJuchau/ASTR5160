a7ff6ea14afa69055d9a61de368bf367e40ca352
Taylor Juchau-Peraza <hawkofthemorning@gmail.com>
2025-03-05 14:56:50 -0700

    added week7 and fixed some file structures (added __init__.py files)

---
+++ Week7/Class14_notebook.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+cd .. #TJ this changes my working directory to be the entire ASTRO5160 folder (instead of just the week7 folder)
+
+
+# In[6]:
+
+
+import os
+import sys
+from Week5.Class10_notebook import * #TJ This imports the functions from class10, which is in week5 directory
+
+
+# In[8]:
+
+
+define_cap(1,1,1) #TJ this verifies that this worked, since this cap function is from a .py file from another directory
+
+
+# In[ ]:
+
+
+
+
